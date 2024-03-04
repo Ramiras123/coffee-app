cf5686fb4b9a78824f05f96ace2e70bbac692327
Aleksandr <smirnow201494@gmail.com>
2024-03-05 01:23:17 +0300

    верстка layout

--- coffee-app/App.tsx
+++ coffee-app/App.tsx
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	Button,
+	Dimensions,
+	ImageBackground,
+	Pressable,
+	StyleSheet,
+	Text,
+	View
+} from 'react-native';
+import { Colors, FontSize, Gaps, Radius } from './shared/tokens';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const d = Dimensions.get('window');
+	return (
+		<View style={styles.container}>
+			<ImageBackground
+				source={require('./assets/backgraund.png')}
+				style={{
+					...styles.image,
+					width: d.width + 46 * 2,
+					marginTop: -(d.height / 100) * 53
+				}}
+				resizeMode="contain"
+			>
+				<View style={styles.content}>
+					<View>
+						<Text style={styles.title}>
+							Одно из самых вкусных кофе в городе!
+						</Text>
+						<Text style={styles.description}>
+							Свежие зёрна, настоящая арабика и бережная обжарка
+						</Text>
+					</View>
+					<Pressable style={styles.btn}>
+						<Text style={styles.text}>Начать</Text>
+					</Pressable>
+				</View>
+			</ImageBackground>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		backgroundColor: Colors.dark,
+		flex: 1,
+
+		justifyContent: 'flex-end'
+	},
+	btn: {
+		borderRadius: Radius.r16,
+		backgroundColor: Colors.primary,
+		paddingHorizontal: 109,
+		paddingVertical: 21
+	},
+	text: {
+		fontSize: FontSize.f16,
+		color: Colors.light,
+		fontWeight: '600'
+	},
+	image: {
+		flex: 1,
+		alignSelf: 'center',
+		justifyContent: 'flex-end'
+	},
+	title: {
+		color: Colors.light,
+		fontSize: FontSize.f34,
+		fontWeight: '600',
+		textAlign: 'center',
+		lineHeight: 43,
+		letterSpacing: 0.4
+	},
+	description: {
+		color: Colors.gray,
+		fontSize: FontSize.f14,
+		textAlign: 'center',
+		letterSpacing: 0.4
+	},
+	content: {
+		gap: Gaps.g24,
+		marginHorizontal: 46,
+		alignItems: 'center',
+		paddingHorizontal: 30,
+		paddingVertical: 41
+	}
 });
---
+++ coffee-app/shared/tokens.ts
@@ -0,0 +1,21 @@
+export const Colors = {
+	dark: '#000000',
+	light: '#FFFFFF',
+	gray: '#A9A9A9',
+	primary: '#C67C4E',
+	primaryHover: '#A76237'
+};
+
+export const Radius = {
+	r16: 16
+};
+
+export const FontSize = {
+	f34: 34,
+	f14: 14,
+	f16: 16
+};
+
+export const Gaps = {
+	g24: 24
+};
